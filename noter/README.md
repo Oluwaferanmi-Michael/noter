e134286dc318ca20e839d107223ea259cc800f57
Oluwaferanmi-Michael <aechealgr8@gmail.com>
2023-09-09 12:46:45 -0400

    noter start

---
+++ noter/README.md
@@ -0,0 +1,14 @@
+# noter
+
+A new Flutter project.
+
+## Lets make a notes app
+
+### What we need to achieve
+
+<ol>  
+    <li> Create UI </li>
+    <li> Find a way to store the data</li>
+    <li> Be awesome</li>
+</ol>
+    
---
+++ noter/lib/views/intro/name_request_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NameRequestPage extends StatelessWidget {
+  const NameRequestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
